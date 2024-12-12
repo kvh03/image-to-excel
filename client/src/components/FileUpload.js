6111142e3fd0e74bbe4cf8ce40e67b7da614bbc6
kvh <101593990+kvh03@users.noreply.github.com>
2024-12-12 14:12:00 +0530

    Update FileUpload.js

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -79,7 +79,7 @@ const FileUpload = React.memo(() => {
         formData.append('title', title);
 
         try {
-            const response = await axios.post('http://localhost:5000/upload', formData, {
+            const response = await axios.post('https://image-to-excel.onrender.com/upload', formData, {
                 headers: { 'Content-Type': 'multipart/form-data' },
             });
 
